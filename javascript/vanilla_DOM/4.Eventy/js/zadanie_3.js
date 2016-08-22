257c8f870078aa4706e72a81fd2ac62e94e419b5
karlola777 <karlola777@gmail.com>
2016-08-22 19:55:59 +0200

    dodanie kolejnej "różności" w javascript

--- javascript/vanilla_DOM/4.Eventy/js/zadanie_3.js
+++ javascript/vanilla_DOM/4.Eventy/js/zadanie_3.js
@@ -3,20 +3,21 @@
  * dowolną liczbę html-owych zestawów button i span
  * (dlatego rozwiązanie jest takie brudne) :( */
 document.addEventListener("DOMContentLoaded", function () {
-    var allButtons = document.querySelectorAll('button');
-    var allCounts = new Array(allButtons.length).fill(0);
+    var allButtons = document.getElementsByTagName('button');
     for(var i = 0; i < allButtons.length; i++) {
-        allButtons[i].addEventListener('click', function () {
-            /*"i" z pętli podane do funkcji powyżej
-            z jakiegoś powodu nie jest liczbą
-            - chyba całym przyciskiem???
-             */
-            //pobieram liczbę z html-owego id przycisku
-            var btnId = parseInt(this.id.replace('button',''));
-            allCounts[btnId-1] += 1;//to funkcja widzi z nadrzędnej
-            var counter = document.querySelector('#'+this.id+' + p > span');
-            counter.innerHTML = allCounts[btnId-1];
-
-        });
+        (function(index) {
+            allButtons[i].addEventListener('click', function () {
+                document.querySelectorAll('span.counter')[index].innerHTML++;
+                //var allCounts = document.getElementsByClassName('counter');
+                //allCounts[1]=5;
+                
+                //pobieram liczbę z html-owego id przycisku
+                //var btnId = parseInt(this.id.replace('button',''));
+                /*var btnId = this.id;
+                 allCounts[btnId-1] += 1;//to funkcja widzi z nadrzędnej
+                 var counter = document.querySelector('#'+this.id+' + p > span');
+                 counter.innerHTML = allCounts[btnId-1];*/
+            });
+        })(i);
     }
 });
--- javascript/vanilla_DOM/4.Eventy/js/zadanie_5.js
+++ javascript/vanilla_DOM/4.Eventy/js/zadanie_5.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     var allBoxes = document.querySelectorAll('.box');
     for (var i=0; i<allBoxes.length;i++){
-        allBoxes[i].addEventListener('click', function () {
+        allBoxes[i].addEventListener('mouseover', function () {
             var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
             this.style.backgroundColor=randomColor;
         })
